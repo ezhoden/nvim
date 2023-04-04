eae2bc95c695bf395fcac26b7c39944c25b8287e
Denis Ezhov <ezhoden@mail.ru>
2023-04-04 23:49:38 +0400

    moved to kickstart.nvim config

--- lua/ezhoden/init.lua
+++
@@ -1,8 +0,0 @@
-require("ezhoden.remap")
-require("ezhoden.set")
-
-vim.cmd.colorscheme "gruvbox-material"
-
--- settings for leetcode
-vim.g.leetcode_browser = "edge"
-vim.g.leetcode_solution_filetype = "typescript"
